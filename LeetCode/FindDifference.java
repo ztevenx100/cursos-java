4edf19caafe32d6d9951a74337cfb3220ec0efbf
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2025-02-17 17:59:42 -0500

    TASK(LeetCode): Add exercise 20

---
+++ LeetCode/FindDifference.java
@@ -0,0 +1,50 @@
+/*
+ * 2215. Find the Difference of Two Arrays
+ * Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+ * answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+ * answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+ * Note that the integers in the lists may be returned in any order.
+*/
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        // Convertimos los arreglos en conjuntos para eliminar duplicados
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        // Encontramos los elementos en set1 que no están en set2
+        List<Integer> diff1 = new ArrayList<>();
+        for (int num : set1) {
+            if (!set2.contains(num)) {
+                diff1.add(num);
+            }
+        }
+
+        // Encontramos los elementos en set2 que no están en set1
+        List<Integer> diff2 = new ArrayList<>();
+        for (int num : set2) {
+            if (!set1.contains(num)) {
+                diff2.add(num);
+            }
+        }
+
+        // Construimos la respuesta
+        List<List<Integer>> answer = new ArrayList<>();
+        answer.add(diff1);
+        answer.add(diff2);
+
+        return answer;
+    }
+}
